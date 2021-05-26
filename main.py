4d05298e9a9475d07c3460aeb6421043d938f8fd
Aiara Lobo Gomes <aiara.lobogomes@maastro.nl>
2021-05-26 05:04:00 +0200

    Save processed data & processed data with dummy variables

--- main.py
+++ main.py
@@ -36,10 +36,10 @@ def load_data(cwd):
         'A9a': 'number_diseases',
         'A12a': 'kidney_function', # numerical -> categories
         'A14a': 'hospital_visits', # numerical -> categories, last 12 months
-        'A150a': 'specialties_mumc', # split categories, last 12 months
+        'A150a': 'specialisms_mumc', # split categories, last 12 months
         'A16a': 'hospitalization', # yes/no last 12 months
         'A18a': 'n_ae_visits', # last 12 months
-        'C1a': 'specialties_visits', # CHECK THAT ONE C1
+        'C1a': 'specialisms_visits', # CHECK THAT ONE C1
         'C3a': 'clinic_visit_frequency', # 1st, 2nd, 3rd+ times
         'B1a': 'n_medications', # numerical
         'B7a': 'risky_substances', # split categories
@@ -52,7 +52,7 @@ def load_data(cwd):
         'A13b': 'alergies', # yes/no
         'A20b': 'alergy_complaints', # free text
         'A140b': 'visit_other_hospital', # yes/no
-        'A150b': 'specialties_not_mumc', # split categories
+        'A150b': 'specialisms_not_mumc', # split categories
         'A16b': 'hospitalization_not_mumc', # yes/no
         'A18b': 'n_ae_visits_not_mumc', # numerical
         'B2b': 'self_medication', # split? categories
@@ -86,9 +86,9 @@ def load_data(cwd):
     # Split columns
     split_cols = {
         'current_diseases': np.arange(1, 9),
-        'specialties_mumc': np.arange(1, 20),
+        'specialisms_mumc': np.arange(1, 20),
         'risky_substances': np.arange(1, 13),
-        'specialties_not_mumc': np.arange(1, 20),
+        'specialisms_not_mumc': np.arange(1, 20),
         'self_medication': np.arange(1, 8)
     }
     for col, cats in split_cols.items():
@@ -96,19 +96,6 @@ def load_data(cwd):
             new_col = '%s_%d' % (col, i)
             df[new_col] = df[col].apply(lambda x: 1 if str(i) in str(x) else 0)
         df.drop(columns=[col], inplace=True)
-
-    # Get dummy variables for other categorical columns
-    dummy_cols = ['gender', 'hospitalization', 'presence_risky_substance',
-                  'housing_type', 'using_medication',
-                  'pharmacy_package_availability', 'medication_giver',
-                  'education_level', 'alergies', 'visit_other_hospital',
-                  'hospitalization_not_mumc', 'health_assessement',
-                  'medication_prescription', 'prescribed_new_medication',
-                  'stop_medication', 'no_drug_intervention', 'evs_conflict',
-                  'medication_interactions', 'g_standard_code1',
-                  'wfg_evaluation1', 'g_standard_code2', 'wfg_evaluation2',
-                  'g_standard_code3', 'wfg_evaluation3']
-    df = pd.get_dummies(df, columns=dummy_cols)
 
     return df
 
@@ -122,5 +109,20 @@ if __name__ == '__main__':
     df = load_data(cwd)
     df = df[~df['changed_police_based_reconciliation'].isnull()]
     df.to_csv('data/med_recon.csv', encoding='utf-8', index=False)
+
+    # Get dummy variables
+    dummy_cols = ['gender', 'hospitalization', 'presence_risky_substance',
+                  'housing_type', 'using_medication',
+                  'pharmacy_package_availability', 'medication_giver',
+                  'education_level', 'alergies', 'visit_other_hospital',
+                  'hospitalization_not_mumc', 'health_assessement',
+                  'medication_prescription', 'prescribed_new_medication',
+                  'stop_medication', 'no_drug_intervention', 'evs_conflict',
+                  'medication_interactions', 'g_standard_code1',
+                  'wfg_evaluation1', 'g_standard_code2', 'wfg_evaluation2',
+                  'g_standard_code3', 'wfg_evaluation3']
+    df = pd.get_dummies(df, columns=dummy_cols)
+    df.to_csv('data/med_recon_dummies.csv', encoding='utf-8', index=False)
+
 
 
